04f0b6a1746855bdc18fbd2ae52d5fbfc64c10d2
jszafran <jszafran.pv@gmail.com>
2021-01-17 10:38:24 +0100

    Script for XML -> dataframe (csv) transformation.

--- scripts/transform_data_to_df.py
+++ scripts/transform_data_to_df.py
@@ -20,11 +20,33 @@ TENORS = [
     "BC_30YEAR",
 ]
 
+DF_COLUMNS = [
+    "date",
+    "1m",
+    "2m",
+    "3m",
+    "6m",
+    "1y",
+    "2y",
+    "3y",
+    "5y",
+    "7y",
+    "10y",
+    "20y",
+    "30y",
+]
+
 
 def parse_xml_row(xml_tag: bs4.element.Tag) -> List:
+    """
+    :param xml_tag: Tag representing a single entry in XML file.
+    Contains curve data for one specific day.
+    :return: List containing parsed data and curve data values.
+    """
     date = datetime.datetime.strptime(
         xml_tag.content.NEW_DATE.text[:10], "%Y-%m-%d"
     ).date()
+    # TODO: check if there's better way to access tag attribute with variable
     curve_data = [
         float(getattr(xml_tag.content, tenor).text)
         if getattr(xml_tag.content, tenor).text
@@ -35,34 +57,38 @@ def parse_xml_row(xml_tag: bs4.element.Tag) -> List:
 
 
 def parse_xml_data() -> None:
-    year = 2019
-    with open(settings.RAW_CURVES_DATA_DIR / f"{year}.xml", "rt") as f:
-        xml_data = f.read()
+    year_from = 1990
+    year_until = 2020
+    dfs = []
 
-    soup = bs4.BeautifulSoup(xml_data, "xml")
+    for year in range(year_from, year_until + 1):
+        with open(settings.RAW_CURVES_DATA_DIR / f"{year}.xml", "rt") as f:
+            xml_data = f.read()
 
-    parsed_rows = [parse_xml_row(row) for row in soup.find_all("entry")]
+        soup = bs4.BeautifulSoup(xml_data, "xml")
 
-    df = pd.DataFrame(
-        columns=[
-            "date",
-            "1m",
-            "2m",
-            "3m",
-            "6m",
-            "1y",
-            "2y",
-            "3y",
-            "5y",
-            "7y",
-            "10y",
-            "20y",
-            "30y",
-        ],
-        data=parsed_rows,
+        parsed_rows = [parse_xml_row(row) for row in soup.find_all("entry")]
+
+        df = pd.DataFrame(
+            columns=DF_COLUMNS,
+            data=parsed_rows,
+        )
+
+        dfs.append(df)
+        print(f"Year {year} done.")
+
+    # post-merge processing
+    all_years_df = pd.concat(dfs, axis=0)
+    all_years_df.set_index("date", inplace=True)
+    all_years_df.sort_index(inplace=True)
+
+    years_range_str = f"{year_from}-{year_until}"
+    print(f"Year(s) {years_range_str} parsed successfully.")
+
+    all_years_df.to_csv(
+        settings.PARSED_DATA_DIR / f"{years_range_str}.csv",
+        index=True,
     )
-
-    df.set_index("date", inplace=True)
 
 
 if __name__ == "__main__":
