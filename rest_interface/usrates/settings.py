705855d048a5f0d5874ab1f769de6f672de8ce28
jszafran <jszafran.pv@gmail.com>
2021-01-21 09:04:23 +0100

    Adding Rest Framework + Rates app into project.

--- rest_interface/usrates/settings.py
+++ rest_interface/usrates/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
+
+USER_APPS = [
+    "rates.apps.RatesConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + USER_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
